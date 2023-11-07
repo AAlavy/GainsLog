7129dd6744387a39702684f802691efe599a6a5c
Middlo <sebastian_baszcz@yahoo.co.uk>
2023-11-08 00:28:07 +0100

    Basic Implementations for Create Program

--- Test/CreateProgramPage.xaml.cs
+++ Test/CreateProgramPage.xaml.cs
@@ -4,7 +4,12 @@ using System.Reflection;
 using System.Text.Json;
 
 public partial class CreateProgramPage : ContentPage
-{
+{ 
+    ExerciseViewModel pickerModel;
+    ExerciseViewModel viewModel;
+    List<Exercise> exercises;
+    string selectedDate;
+
     public CreateProgramPage()
     {
         InitializeComponent();
@@ -13,20 +18,46 @@ public partial class CreateProgramPage : ContentPage
 
     private void LoadExercises()
     {
-        var filePath = Path.Combine(AppContext.BaseDirectory, "Resources/Programs/Exercises.json");
+        var filePath = Path.Combine(AppContext.BaseDirectory, "Resources/Exercises/Exercises.json");
         string json = File.ReadAllText(filePath);
         ExerciseList exerciseList = JsonSerializer.Deserialize<ExerciseList>(json);
-        List<Exercise> exercises = exerciseList.Exercises;
+        exercises = exerciseList.Exercises;
 
-        foreach (Exercise exercise in exercises)
+        pickerModel = new ExerciseViewModel();
+        if (exercises != null)
         {
-            Layout.Children.Add(new Label
+            foreach (Exercise exercise in exercises)
             {
-                Text = exercise.Name
-            });
+                pickerModel.AddPicker(exercise.Name);
+            }
         }
+        BindingContext = pickerModel;
     }
 
+    private void btnSubmit_Clicked(object sender, EventArgs e)
+    {
+        string exercise = picker.Items[picker.SelectedIndex];
+        string numReps = entryRep.Text;
+        string numSets = entrySet.Text;
+
+        pickerModel.AddExercise(exercise, numReps, numSets);
+        
+        //dateSelectedLabel.Text = button.Text;
+    }
+
+    private void btnDay_Clicked(object sender, EventArgs e)
+    {
+        Button button = (Button)sender;
+        selectedDate = button.Text;
+    }
+
+    private async void ListView_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+    {
+        var exercise = exercises.ElementAt(e.SelectedItemIndex);
+        //exerciseSelectedLabel.Text = exercise.Name;
+    }
+
+    /*
     private void OnDrag(object sender, DragStartingEventArgs e)
     {
         e.Data.Properties.Add("Frame", myFrame);
@@ -40,4 +71,5 @@ public partial class CreateProgramPage : ContentPage
             myDropTarget.Content = frame.Content;
         }
     }
+    */
 }
---
+++ Test/ExerciseViewModel.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+
+    public class ExerciseRows
+    {
+        private string _exercise = string.Empty;
+        private string _reps = string.Empty;
+        private string _sets = string.Empty;
+        public string Exercise
+        {
+            get { return _exercise; }
+            set { _exercise = value; }
+        }
+
+        public string Reps
+        {
+            get { return _reps; }
+            set { _reps = value; }
+        }
+
+        public string Sets
+        {
+            get { return _sets; }
+            set { _sets = value; }
+        }
+
+        public ExerciseRows(string exercise, string reps, string sets)
+        {
+            _exercise = exercise;
+            _reps = reps;
+            _sets = sets;
+        }
+    }
+
+    public class ExerciseViewModel
+    {
+
+        private ObservableCollection<ExerciseRows> exerciseCollection = new ObservableCollection<ExerciseRows>();
+        private ObservableCollection<string> exercisePickerCollection = new ObservableCollection<string>();
+
+        public ExerciseViewModel()
+        {
+
+        }
+
+        public void AddPicker(string project)
+        {
+            exercisePickerCollection.Add(project);
+        }
+
+        public void AddExercise(string exercise, string reps, string sets)
+        {
+            exerciseCollection.Add(new ExerciseRows(exercise, reps, sets));
+        }
+
+        public ObservableCollection<ExerciseRows> ExerciseCollection
+        {
+            get { return exerciseCollection; }
+            set { exerciseCollection = value; }
+        }
+
+        public ObservableCollection<string> ExercisePickerCollection
+        {
+            get { return exercisePickerCollection; }
+            set { exercisePickerCollection = value; }
+        }
+    }
+}
