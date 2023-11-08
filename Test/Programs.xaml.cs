db58bb7eeae29c1fd454836bf0701efdae936c28
Andy Alavinasab <andy.alavinasab@gmail.com>
2023-11-08 01:16:59 +0100

    Creates clickable frames

--- Test/Programs.xaml.cs
+++ Test/Programs.xaml.cs
@@ -15,14 +15,50 @@ public partial class Programs : ContentPage
     {
         string folderPath = Path.Combine(AppContext.BaseDirectory, "Resources/Programs");
         string[] files = Directory.GetFiles(folderPath);
+
+        int row = 0;
+        int col = 0;
         foreach (string file in files)
         {
             string json = File.ReadAllText(file);
             WorkoutProgram program = JsonSerializer.Deserialize<WorkoutProgram>(json);
-            ProgramGrid.Children.Add(new Label { 
+
+            Frame workoutFrame = new Frame
+            {
+                BorderColor = Color.FromRgb(255, 255, 255),
+                BackgroundColor = Color.FromRgb(0, 0, 0),
+                CornerRadius = 10,
+                HeightRequest = 60,
+                WidthRequest = 60,
+
+            };
+            workoutFrame.Content = (new Label
+            {
                 Text = program.Name
             });
 
+            Grid.SetRow(workoutFrame, row);
+            Grid.SetColumn(workoutFrame, col++);
+
+            if (col == 3)
+            {
+                col = 0;
+                row++;
+            }
+
+            var programTap = new TapGestureRecognizer();
+            programTap.Tapped += (s, e) =>
+            {
+                workoutFrame.BackgroundColor = Color.FromRgb(255, 0, 0);
+            };
+
+            workoutFrame.GestureRecognizers.Add(programTap);
+
+
+
+            ProgramGrid.Children.Add(workoutFrame);
+
         }
     }
+
 }
